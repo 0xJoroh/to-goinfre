83c890ba6fb1ac576e125941f004456d33c8a9d0
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2021-07-05 14:12:30 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-echo "Installing brew ..."
+echo "\n\e[92mInstalling ZSH ...\e[39m"
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "\n\e[92mInstalling brew ...\e[39m"
 brew > brew_check 2>&1
 check=$(cat brew_check |  wc -l | tr -d " ")
 if [ $check -lt 5 ]
@@ -12,12 +15,13 @@ then
 fi
 rm brew_check
 
-echo "Installing yarn with its dependencies... (node, nghttp2, openssl, jemalloc, libev, ...)"
+echo "alias code='/goinfre/mait-si-/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code'" >> ~/.zshrc
+
+echo "\n\e[92mInstalling yarn with its dependencies... (node, nghttp2, openssl, jemalloc, libev, ...)\e[39m"
 brew install yarn
 
-echo "clone Reop"
-cd /goinfre/$(whoami)
-git clone https://github.com/ayour-labs/modern-stream.git
+echo "\n\e[92mCloning Moder Stream...\e[39m"
+git clone https://github.com/ayour-labs/modern-stream.git /goinfre/$(whoami)/modern-stream
 
-echo "Run Modern Stream Project"
+echo "\n\e[92mRun Modern Stream Project\e[39m"
 cd /goinfre/$(whoami)/modern-stream && code . && yarn install && yarn start
